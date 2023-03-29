5a88adbef451c306f5254ca8af881890afd2d9e7
MoyusiteruIori <y.meng1@outlook.com>
2023-03-29 21:01:30 +0800

    编写虚拟机指令集

--- mycmmi.c
+++ mycmmi.c
@@ -2,11 +2,44 @@
 #include <stdlib.h>
 #include <memory.h>
 #include <string.h>
+#define int long long
 
 int token;              // 当前 token
 char *src, *old_src;    // 源代码
 int poolsize;           // text/data/stack 段的默认大小
 int line;               // 行号
+
+// +------------------+
+// |    stack   |     |      high address
+// |    ...     v     |
+// |                  |
+// |                  |
+// |                  |
+// |                  |
+// |    ...     ^     |
+// |    heap    |     |
+// +------------------+
+// | bss  segment     |
+// +------------------+
+// | data segment     |
+// +------------------+
+// | text segment     |      low address
+// +------------------+
+
+int *text,
+    *old_text,
+    *stack;
+char *data;
+
+int *pc,    
+    *bp,    // 基址指针，指向栈顶某些位置
+    *sp,    // 栈顶
+     ax,    // 通用寄存器
+     cycle;
+
+enum { LEA, IMM, JMP, CALL, JZ, JNZ, ENT, ADJ, LEV, LI, LC, SI, SC, PUSH,
+       OR,  XOR, AND, EQ, NE, LT, GT, LE, GE, SHL, SHR, ADD, SUB, MUL, DIV, MOD ,
+       OPEN, READ, CLOS, PRTF, MALC, MSET, MCMP, EXIT };
 
 // 词法分析，获取下一个 token
 void next() {
@@ -29,10 +62,61 @@ void program() {
 
 // 虚拟机入口
 int eval() {
+    int op, *tmp;
+    while (1) {
+        op = *pc++; // get next operation code
+
+        if (op == IMM)       {ax = *pc++;}
+        else if (op == LC)   {ax = *(char *)ax;}
+        else if (op == LI)   {ax = *(int *)ax;}
+        else if (op == SC)   {ax = *(char *)*sp++ = ax;}
+        else if (op == SI)   {*(int *)*sp++ = ax;}
+        else if (op == PUSH) {*--sp = ax;}
+        else if (op == JMP)  {pc = (int *)*pc;}
+        else if (op == JZ)   {pc = ax ? pc + 1 : (int *)*pc;}
+        else if (op == JNZ)  {pc = ax ? (int *)*pc : pc + 1;}
+        else if (op == CALL) {*--sp = (int)(pc+1); pc = (int *)*pc;}
+        else if (op == ENT)  {*--sp = (int)bp; bp = sp; sp = sp - *pc++;}
+        else if (op == ADJ)  {sp = sp + *pc++;}
+        else if (op == LEV)  {sp = bp; bp = (int *)*sp++; pc = (int *)*sp++;}
+        else if (op == LEA)  {ax = (int)(bp + *pc++);}
+        else if (op == OR)  ax = *sp++ | ax;
+        else if (op == XOR) ax = *sp++ ^ ax;
+        else if (op == AND) ax = *sp++ & ax;
+        else if (op == EQ)  ax = *sp++ == ax;
+        else if (op == NE)  ax = *sp++ != ax;
+        else if (op == LT)  ax = *sp++ < ax;
+        else if (op == LE)  ax = *sp++ <= ax;
+        else if (op == GT)  ax = *sp++ >  ax;
+        else if (op == GE)  ax = *sp++ >= ax;
+        else if (op == SHL) ax = *sp++ << ax;
+        else if (op == SHR) ax = *sp++ >> ax;
+        else if (op == ADD) ax = *sp++ + ax;
+        else if (op == SUB) ax = *sp++ - ax;
+        else if (op == MUL) ax = *sp++ * ax;
+        else if (op == DIV) ax = *sp++ / ax;
+        else if (op == MOD) ax = *sp++ % ax;
+        else if (op == EXIT) { printf("exit(%d)\n", *sp); return *sp;}
+        else if (op == OPEN) { ax = open((char *)sp[1], sp[0]); }
+        else if (op == CLOS) { ax = close(*sp);}
+        else if (op == READ) { ax = read(sp[2], (char *)sp[1], *sp); }
+        else if (op == PRTF) { tmp = sp + pc[1]; ax = printf((char *)tmp[-1], tmp[-2], tmp[-3], tmp[-4], tmp[-5], tmp[-6]); }
+        else if (op == MALC) { ax = (int)malloc(*sp);}
+        else if (op == MSET) { ax = (int)memset((char *)sp[2], sp[1], *sp);}
+        else if (op == MCMP) { ax = memcmp((char *)sp[2], (char *)sp[1], *sp);}
+        else {
+            printf("unknown instruction:%d\n", op);
+            return -1;
+        }
+    }
     return 0;
 }
 
+#undef int
+
 int main(int argc, char **argv) {
+
+#define int long long
     int i, fd;
 
     argc--;
@@ -58,6 +142,42 @@ int main(int argc, char **argv) {
     src[i] = 0;
     close(fd);
 
+    // 为虚拟机分配内存
+    if (!(text = old_text = malloc(poolsize))) {
+        printf("could not malloc(%d) for text area\n", poolsize);
+        return -1;
+    }
+
+    if (!(data = malloc(poolsize))) {
+        printf("could not malloc(%d) for data area\n", poolsize);
+        return -1;
+    }
+
+    if (!(stack = malloc(poolsize))) {
+        printf("could not malloc(%d) for stack area\n", poolsize);
+        return -1;
+    }
+
+    memset(text, 0, poolsize);
+    memset(data, 0, poolsize);
+    memset(stack, 0, poolsize);
+
+    bp = sp = (int *)((int)stack + poolsize);
+    ax = 0;
+
+    i = 0;
+    text[i++] = IMM;
+    text[i++] = 10;
+    text[i++] = PUSH;
+    text[i++] = IMM;
+    text[i++] = 20;
+    text[i++] = ADD;
+    text[i++] = PUSH;
+    text[i++] = EXIT;
+    pc = text;
+
     program();
     return eval();
+
+#undef int 
 }
